b0c0ee028713fcd0820e21e62939f94de0b16f95
Mathew May <mathewm@hotmail.co.nz>
2025-09-03 09:36:58 +0200

    AE-57 React Poc: Add git ignores

--- public/calendar/react/main.jsx
+++ public/calendar/react/main.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+// This has our "Themes" dark, light, high-contrast.
+// It could be swapped to a App level CSS based on our "Theme" css.
 import "./styles.css";
+// The wrapper around the entire app that provides the theme context as well as the theme select UI.
 import Home from "./component/home";
 import { ThemeProvider } from "./context/themeProvider";
 
@@ -10,9 +13,10 @@ function init(selector, props) {
     const container = document.querySelector(selector);
     if (container) {
         const root = createRoot(container);
-        root.render(<ThemeProvider><div className="App">
+        root.render(<ThemeProvider>
             <Home />
-        </div><App {...props} /></ThemeProvider>);
+            <App {...props} />
+        </ThemeProvider>);
     }
 }
 

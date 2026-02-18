167ec163a5531ff2be88ac1954e8da3a6ddfff46
Adrian Greeve <adrian@moodle.com>
2026-02-18 14:27:40 +0800

    AE-81: Removed test code
    We have a local plugin to see how this all works.

--- public/mod/book/view.php
+++ public/mod/book/view.php
@@ -120,11 +120,6 @@ if (!$chapterid) {
 
     echo $OUTPUT->header();
 
-    echo $OUTPUT->render_from_template('mod_book/book-test', []);
-    $PAGE->requires->js_type_module('mod_book/travel');
-    $PAGE->requires->react_type_module('core_ai/aitravel');
-    echo $OUTPUT->render_from_template('mod_book/jshelper-test', []);
-
     $renderer = $PAGE->get_renderer('mod_book');
     $actionmenu = new \mod_book\output\main_action_menu($cm->id, $chapters, $chapter, $book);
     $renderedmenu = $renderer->render($actionmenu);
